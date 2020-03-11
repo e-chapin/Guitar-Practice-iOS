1cd20c3a1257abb45e084cc471d01ea4dc8ce44c
Eric <e-jameson@bethel.edu>
2020-03-10 21:28:01 -0500

    Initial Commit

---
+++ Guitar Practice/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Guitar Practice
+//
+//  Created by Eric Jameson on 3/10/20.
+//  Copyright © 2020 Eric Jameson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Guitar Practice/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Guitar Practice
+//
+//  Created by Eric Jameson on 3/10/20.
+//  Copyright © 2020 Eric Jameson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
